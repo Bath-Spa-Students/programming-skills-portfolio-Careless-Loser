0e3ef2a7ebd324f1cf3af8f228780dab9b973c14
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-09-22 14:41:30 +0400

    Update Exercise_2.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise_2.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_2.py
@@ -1,5 +1,8 @@
+# Import the 'sys' module to access Python system-related information.
 import sys
+# Printing the Python version.
 print("Python version")
 print(sys.version)
+# Print Python version information in a structured format.
 print("Version info.")
 print(sys.version_info)
