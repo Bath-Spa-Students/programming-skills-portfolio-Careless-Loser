b2f697cbe5b50e1f4ccb698baffe96e5270bd3a3
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-09-22 14:41:36 +0400

    Create Exercise_4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_4.py
@@ -0,0 +1,8 @@
+#First line is defining the three strings in different variables
+string_1 = "I" 
+string_2 = " love"
+string_3 = " code lab 1 <3"
+#combining the strings
+result = string_1 + string_2 + string_3
+#printing the combined string
+print(result)
