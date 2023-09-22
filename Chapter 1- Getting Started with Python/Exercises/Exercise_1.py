f3bf0b1353d0c8754be8970358c52fcd4fadd17f
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-09-22 14:41:24 +0400

    Update Exercise_1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise_1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_1.py
@@ -1,9 +1,13 @@
+# Define a function named 'print_twinkle' to print the "Twinkle, Twinkle, Little Star" poem.
 def print_twinkle():
+    # Print the first two lines of the poem with proper indentation.
     print("Twinkle, twinkle, little star,")
     print("\tHow I wonder what you are!")
+        # Print the next two lines of the poem with increasing indentation.
     print("\t\tUp above the world so high,")
     print("\t\tLike a diamond in the sky.")
+        # Print the last two lines of the poem with proper indentation.
     print("Twinkle, twinkle, little star,")
     print("\tHow I wonder what you are!")
-
+# Call the 'print_twinkle' function to print the poem.
 print_twinkle()
