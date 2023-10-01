522d9aa4088b100fe39929315cdbaf8db6649b3a
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-01 17:00:12 +0400

    Create Exercise_2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_2.py
@@ -0,0 +1,8 @@
+#Defineing a variable with name of a famous person
+Famous_person = "Ada Lovelace"
+#Creatong a froematted string using a f string which includes the
+#famous person variable and the qoute (single quotes are for outer string because)
+#it will allow double quotes for the saying 
+Message = f'{Famous_person.title()} once said, "That brain of mine is something more than merely mortal; as time will show."'
+#Print the message
+print(Message)
