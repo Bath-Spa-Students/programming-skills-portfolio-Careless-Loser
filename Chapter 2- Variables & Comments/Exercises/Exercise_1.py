7cc09189f0b7f31ed367289ac6a0901bf7a30a05
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-01 17:00:03 +0400

    Create Exercise_1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_1.py
@@ -0,0 +1,9 @@
+#We will give a name to a variable first.
+#Let it name be Message
+Message = "I love Bath Spa University!"
+#Now we will print this message
+print(Message)
+#Now we will print a new message on the same variable
+Message = "I love Code Lab 1!"
+#Printing the new message
+print(Message)
