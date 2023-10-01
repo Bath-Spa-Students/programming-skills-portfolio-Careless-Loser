fa3385d79506d6da46acc00423a1762835ec98a2
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-01 17:00:26 +0400

    Create Exercise_4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_4.py
@@ -0,0 +1,6 @@
+#Defining the variable with favorite number
+Favorite_Number = 8
+#Create a message using f-string to include the favorite number
+Message = f"My favorite number is {Favorite_Number}."
+#Print the message given
+print(Message)
