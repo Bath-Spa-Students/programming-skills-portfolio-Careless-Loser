68e648ef05dfa0df0d1066629287e2e30203b3f7
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-02 12:03:10 +0400

    Create Question_1.py

---
+++ Chapter 1- Getting Started with Python/Practice Questions Exercise/Question_1.py
@@ -0,0 +1,7 @@
+#Write a program to output the letters 
+#W, X, Y, Z each on a sperate line
+#Frist we will give name to a variable
+#Typing \n so they can be printed on next line
+Letters = "\n W \n X \n Y \n Z"
+#Printing the message
+print (Letters)
