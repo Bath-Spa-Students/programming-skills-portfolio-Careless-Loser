b3b6f4d0a27481cef2621e2d8864bb41a337af5a
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-02 12:03:54 +0400

    Create Question_4.py

---
+++ Chapter 1- Getting Started with Python/Practice Questions Exercise/Question_4.py
@@ -0,0 +1,9 @@
+#Compute the area of triangle
+#Adding input so we can type in the number for 
+#height and the base
+Base = int(input("Enter the number for base: "))
+Height = int(input("Enter the number fot height: "))
+#Writing the formula of triangle
+Area = (Base * Height) / 2
+#Prinitng the answer
+print("The area of triangle is ", Area)
