0633ca0986758dd4d9a38f0ecaa3d549a353d61b
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-02 12:03:43 +0400

    Create Question_3.py

---
+++ Chapter 1- Getting Started with Python/Practice Questions Exercise/Question_3.py
@@ -0,0 +1,5 @@
+#You need to make a program for a leader baord.
+#The program needs to output the number 1 to 9,
+#each on a separate line followed by a dot
+#printing the numbers with dots followed by it and writing \n to break each
+print("\n1. \n2. \n3. \n4. \n5. \n6. \n7. \n8. \n9.")
