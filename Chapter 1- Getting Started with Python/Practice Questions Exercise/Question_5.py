4abfaebf65c7661578c7203522d92396d95970b3
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-02 12:04:03 +0400

    Create Question_5.py

---
+++ Chapter 1- Getting Started with Python/Practice Questions Exercise/Question_5.py
@@ -0,0 +1,11 @@
+#Write four strings in different variables
+#and print the output as one string.
+#First line is defining the three strings in different variables
+string_1 = "I" 
+string_2 = " love"
+string_3 = " code lab 1 "
+string_4 = " <3 "
+#combining the strings
+Strings = string_1 + string_2 + string_3 + string_4
+#printing the combined string
+print(Strings)
