7c1cf463805f53b3e504f5c9541592386aa04b66
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-12 12:16:07 +0400

    Create Exercise_1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise_1.py
@@ -0,0 +1,8 @@
+#Defining a list called 'Name' with three elements
+Name = ['John', 'Sean', 'Reingh']
+#Print the first element of the list (Index 0)
+print(Name[0])
+#Print the second element of the list (Index 1)
+print(Name[1])
+#Print the third element of the list (Index 2)
+print(Name[2])
