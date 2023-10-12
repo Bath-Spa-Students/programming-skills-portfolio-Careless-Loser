93be3bc490d97ad857f3ae1122caaf25a646a90f
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-12 12:16:21 +0400

    Create Exercise_2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise_2.py
@@ -0,0 +1,14 @@
+# Define a list called 'Names' with three elements
+Names = ['John', 'Sean', 'Reingh']
+# Create a message using f-string, addressing the first element of the list with title() to capitalize the first letter
+Message = f"Hello, {Names[0].title()}"
+# Printing the message 
+print(Message)
+# Create a message using f-string, addressing the second element of the list with title() to capitalize the first letter
+Message = f"Hello, {Names[1].title()}"
+# Printing the message 
+print(Message)
+# Create a message using f-string, addressing the third element of the list with title() to capitalize the first letter
+Message = f"Hello, {Names[2].title()}"
+# Printing the message 
+print(Message)
