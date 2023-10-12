ff7729e71338899adaa2395354fa94d486d3b71e
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-12 12:16:36 +0400

    Create Exercise_4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise_4.py
@@ -0,0 +1,14 @@
+# Defining a list of guests
+Guest = ['John D', 'Sean oyean', 'Reighn llyod']
+# Get the title-cased name of the first guest
+Name = Guest[0].title()
+# Print an invitation for the first guest
+print(f"{Name}, please come to dinner.")
+# Get the title-cased name of the second guest
+Name = Guest[1].title()
+# Print an invitation for the second guest
+print(f"{Name}, please come to dinner.")
+# Get the title-cased name of the third guest
+Name = Guest[2].title()
+# Print an invitation for the third guest
+print(f"{Name}, please come to dinner.")
