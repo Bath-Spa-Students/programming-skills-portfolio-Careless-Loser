ac9c367acc1972f4bc4c3fef8ed4694a72ed71e5
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-22 17:57:18 +0400

    Create Exercise_1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise_1.py
@@ -0,0 +1,18 @@
+#Passing Version
+
+#Define the variable Alien_Color and set it to 'Green'.
+Alien_Color = 'Green'
+# Check if the value of Alien_Color is equal to 'Green'.
+if Alien_Color == 'Green':
+    # If it is, print the message "You just earned 5 points!"
+    print("You just earned 5 points!")
+
+#Falling Version
+
+# Define the variable Alien_Color and set it to 'red'.
+Alien_Color = 'red'
+# Check if the value of Alien_Color is equal to 'Green'.
+if Alien_Color == 'Green':
+     # Since Alien_Color is not 'Green', this code block is skipped.
+    # No message is printed in this case.
+    print("You just earned 5 points!")
