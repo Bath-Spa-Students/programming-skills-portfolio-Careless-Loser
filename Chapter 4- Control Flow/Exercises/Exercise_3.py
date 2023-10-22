153d08f53394a031dc53e2f8a302e8ceec5981c7
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-22 17:57:34 +0400

    Create Exercise_3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise_3.py
@@ -0,0 +1,14 @@
+# Define the variable Alien_Color and set it to 'Red'.
+Alien_Color = 'Red'
+# Check if the value of Alien_Color is equal to 'Green'.
+if Alien_Color == 'Green':
+    # If the condition is true (Alien_Color is 'Green'), print "You just earned 5 points!"
+    print("You just earned 5 points!")
+# If the first condition is not met, check if the value is equal to 'Yellow'.
+elif Alien_Color == 'Yellow':
+    # If this condition is true (Alien_Color is 'Yellow'), print "You just earned 10 points!"
+    print("You just earned 10 points!")
+# If neither the first nor the second condition is true, execute the 'else' block.
+else:
+    # If neither condition is true (Alien_Color is not 'Green' or 'Yellow'), print "You just earned 15 points!"
+    print("You just earned 15 points!")
