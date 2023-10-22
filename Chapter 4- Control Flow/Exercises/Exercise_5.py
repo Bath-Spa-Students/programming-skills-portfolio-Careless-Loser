4b719e74a4b188f846aadd2688d569d2f3357ec5
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-22 17:57:51 +0400

    Create Exercise_5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise_5.py
@@ -0,0 +1,22 @@
+# Create a list of favorite fruits.
+favorite_fruits = ['blueberries', 'grapes', 'strawberry']
+# Check if 'bananas' is in the list.
+if 'bananas' in favorite_fruits:
+    # If 'bananas' is in the list, print "You really like bananas!"
+    print("You really like bananas!")
+# Check if 'apples' is in the list.
+if 'apples' in favorite_fruits:
+    # If 'apples' is in the list, print "You really like apples!"
+    print("You really like apples!")
+# Check if 'blueberries' is in the list.
+if 'blueberries' in favorite_fruits:
+    # If 'blueberries' is in the list, print "You really like blueberries!"
+    print("You really like blueberries!")
+# Check if 'kiwis' is in the list.
+if 'kiwis' in favorite_fruits:
+    # If 'kiwis' is in the list, print "You really like kiwis!"
+    print("You really like kiwis!")
+# Check if 'strawberry' is in the list.
+if 'strawberry' in favorite_fruits:
+    # If 'strawberry' is in the list, print "You really like peaches!" (note: the message is incorrect)
+    print("You really like peaches!")
