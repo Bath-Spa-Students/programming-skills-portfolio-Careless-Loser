7faae787f4f3b93082dd0f8276cc11aa6ec2479c
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-22 17:57:25 +0400

    Create Exercise_2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise_2.py
@@ -0,0 +1,23 @@
+#if block runs
+
+# Define the variable Alien_Color and set it to 'Green'.
+Alien_Color = 'Green'
+# Check if the value of Alien_Color is equal to 'Green'.
+if Alien_Color == 'Green':
+    # If the condition is true (Alien_Color is 'Green'), print "You just earned 5 points!"
+    print("You just earned 5 points!")
+else:
+    # If the condition is not true (Alien_Color is not 'Green'), print "You just earned 10 points!"
+    print("You just earned 10 points!")
+
+#else block runs
+
+# Define the variable Alien_Color and set it to 'Yellow'.
+Alien_Color = 'Yellow'
+# Check if the value of Alien_Color is equal to 'Green'.
+if Alien_Color == 'Green':
+    # If the condition is true (Alien_Color is 'Green'), print "You just earned 5 points!"
+    print("You just earned 5 points!")
+else:
+    # If the condition is false (Alien_Color is not 'Green'), print "You just earned 10 points!"
+    print("You just earned 10 points!")
