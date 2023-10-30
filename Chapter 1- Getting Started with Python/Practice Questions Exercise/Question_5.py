0dd03d9f63d0f80aa866c5cc31cf162fffa7190d
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-10-30 23:05:18 +0400

    Update Question_5.py

--- Chapter 1- Getting Started with Python/Practice Questions Exercise/Question_5.py
+++ Chapter 1- Getting Started with Python/Practice Questions Exercise/Question_5.py
@@ -4,7 +4,7 @@
 string_1 = "I" 
 string_2 = " love"
 string_3 = " code lab 1 "
-string_4 = " <3 "
+string_4 = "<3 "
 #combining the strings
 Strings = string_1 + string_2 + string_3 + string_4
 #printing the combined string
