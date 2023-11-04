af9dabe8b4fb65fab5ee5fbc0e17adeb899330bc
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-04 12:52:16 +0400

    Create Question_3.py

---
+++ Chapter 2- Variables & Comments/Practice Questions Exercise/Question_3.py
@@ -0,0 +1,15 @@
+#Write a python program that takes an input from user 
+#and then type cast those values to string, int
+#and float in the separate variables. Print all the variables.
+# Get user input
+User_Input = input("Enter a number: ")
+# Typecast to string
+String_Variable = str(User_Input)
+# Typecast to integer
+Int_Variable = int(User_Input)
+# Typecast to float
+Float_Variable = float(User_Input)
+# Print the variables
+print("As String:", String_Variable)
+print("As Integer:", Int_Variable)
+print("As Float:", Float_Variable)
