b0f456c88ab6c01583998d1316871100af2f8658
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 03:14:30 +0400

    Create Exercise_2.py

---
+++ Chapter 7- Functions/Exercises/Exercise_2.py
@@ -0,0 +1,6 @@
+# Define a function named favorite_book that takes a title as an argument.
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+    print(f"{title} is one of my favorite books.")
+# Call the function with the book title "Child 44."
+favorite_book('Child 44')
