e38444e2822fdb94a91c7db2131ca68aa80c9e37
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 01:59:40 +0400

    Create Question_3.py

---
+++ Chapter 5- Dictionaries/Practice Questions Exercise/Question_3.py
@@ -0,0 +1,11 @@
+#Write a Python program to iterate through the values of a dictionary and print them.
+# Create a sample dictionary
+my_dict = {
+    "Name": "Ifrah",
+    "Age": 18,
+    "Born": 2004,
+    "Birth Place": "Dubai"
+}
+# Iterate through the values and print them
+for value in my_dict.values():
+    print(value)
