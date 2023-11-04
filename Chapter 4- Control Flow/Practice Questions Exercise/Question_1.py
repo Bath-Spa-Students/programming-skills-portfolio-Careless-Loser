a6bbc407c30c9566ac0f2a3e85933058da13062f
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 01:19:43 +0400

    Create Question_1.py

---
+++ Chapter 4- Control Flow/Practice Questions Exercise/Question_1.py
@@ -0,0 +1,17 @@
+#Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the
+#variable z if the variable & is greater than 100.
+# Assuming the variable 'your_variable' has a value
+# Example value for 'your_variable'
+your_variable = 101
+# Check if 'your_variable' is greater than 100
+if your_variable > 100:
+    y = 20  # Assign 20 to 'y' if the condition is met
+    z = 40  # Assign 40 to 'z' if the condition is met
+else:
+    # Assign default values if 'your_variable' is not greater than 100
+    y = 0
+    z = 0
+# Print the values of 'y' and 'z'
+print("y:", y)
+print("z:", z)
+
