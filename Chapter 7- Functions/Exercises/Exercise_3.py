e93302ffbdcaf192a9007800860dc809a6aaaed0
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 03:14:41 +0400

    Create Exercise_3.py

---
+++ Chapter 7- Functions/Exercises/Exercise_3.py
@@ -0,0 +1,9 @@
+# Define a function named make_shirt that takes a size and a message as arguments.
+def make_shirt(size, message):
+    """Summarize the shirt that's going to be made."""
+    # Print a message describing the size and message on the shirt.
+    print(f"\nI'm going to make a {size} t-shirt.")
+    print(f'It will say, "{message}"')
+# Call the function with different size and message values.
+make_shirt('large', 'I love Python!')
+make_shirt(size='medium', message="I love code lab 1 <3")
