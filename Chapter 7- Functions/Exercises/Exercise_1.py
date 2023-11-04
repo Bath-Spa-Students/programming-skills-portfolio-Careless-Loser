021cb94c0a810b04dfaedb052c7d1ddc4b8a209f
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 03:14:24 +0400

    Create Exercise_1.py

---
+++ Chapter 7- Functions/Exercises/Exercise_1.py
@@ -0,0 +1,7 @@
+# Define a function named display_message.
+def display_message():
+    """Display a message about what I'm learning."""
+    msg = "I love Code Lab 1 <3"
+    print(msg)
+# Call the function to display the message.
+display_message()
