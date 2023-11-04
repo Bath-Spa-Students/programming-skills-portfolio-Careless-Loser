b75369fe76d3170ccb7894e7741e44710e9bd585
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-04 17:56:31 +0400

    Update Exercise_3.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise_3.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_3.py
@@ -3,7 +3,7 @@ import datetime
 #The second line is "now" which creates the current datetime object for the current date and time
 now = datetime.datetime.now()
 #The Third line is the print string "Current date and time"
-print (" Current date and time : ")
+print ("Current date and time : ")
 #The fourth line is printing the actual date and time which includes
 #%Y: Year, %m: Month, %d: Day, %H: hour, %M: minute and %S: seconds 
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
