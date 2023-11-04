3ec2ef4a937e7955b963ebb34e9227c66940e798
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 01:59:29 +0400

    Create Question_1.py

---
+++ Chapter 5- Dictionaries/Practice Questions Exercise/Question_1.py
@@ -0,0 +1,13 @@
+#Use a dictionary to store information about yourself
+# Create a dictionary to store information about yourself
+My_Info = {
+    "Name": "Ifrah",
+    "Age": 18,
+    "Born": 2004,
+    "Birth Place": "Dubai"
+}
+# Access and print the information
+print("Name:", My_Info["Name"])
+print("Age:", My_Info["Age"])
+print("Born:", My_Info["Born"])
+print("Birth Place:", My_Info["Birth Place"])
