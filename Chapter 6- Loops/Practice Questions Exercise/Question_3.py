7fc90f65610b21d44bfa76d412ef941efb77cfa3
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 02:55:02 +0400

    Create Question_3.py

---
+++ Chapter 6- Loops/Practice Questions Exercise/Question_3.py
@@ -0,0 +1,8 @@
+#Write a Python program that uses a loop to find the sum of all the numbers from 1 to 100.
+# Initialize a variable to store the sum
+Total = 0
+# Use a loop to add numbers from 1 to 100 to the sum
+for number in range(1, 101):
+    Total += number
+# Print the sum
+print("The sum of numbers from 1 to 100 is:", Total)
