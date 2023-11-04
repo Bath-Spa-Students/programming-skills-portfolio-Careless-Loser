b7b5a52e06a7665b186cdf0f8c24d68ffd4b54f9
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-04 17:01:52 +0400

    Create Question_2.py

---
+++ Chapter 3- Data Structures/Practice Questions Exercise/Question_2.py
@@ -0,0 +1,5 @@
+#Write a statement that creates a list with the following strings: 'Einstein', Newton' , 'Copernicus',
+#and 'Kepler'
+Names_List = ['Einstein', 'Newton', 'Copernicus', 'Kepler']
+# Print the list
+print(Names_List)
