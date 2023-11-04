2c99138f9386745321582533554f073a0829abe2
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 01:59:34 +0400

    Create Question_2.py

---
+++ Chapter 5- Dictionaries/Practice Questions Exercise/Question_2.py
@@ -0,0 +1,11 @@
+#Write a Python program to iterate through the keys of a dictionary and print them
+# Create a sample dictionary
+my_dict = {
+    "Name": "Ifrah",
+    "Age": 18,
+    "Born": 2004,
+    "Birth Place": "Dubai"
+}
+# Iterate through the keys and print them
+for key in my_dict:
+    print(key)
