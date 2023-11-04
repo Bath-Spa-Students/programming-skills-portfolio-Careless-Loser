cc6e4dcc7bdabcec51ec7246c3dbd1c611e147e5
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-04 12:52:30 +0400

    Create Question_4.py

---
+++ Chapter 2- Variables & Comments/Practice Questions Exercise/Question_4.py
@@ -0,0 +1,9 @@
+#Write a python program that stores an integer and string value to variables x and y. Print the type of
+#variable x and y. 
+# Store an integer in variable x
+x = 8
+# Store a string in variable y
+y = "I love code lab 1 <3"
+# Print the types of variables x and y
+print("Type of x:", type(x), x)
+print("Type of y:", type(y), y)
