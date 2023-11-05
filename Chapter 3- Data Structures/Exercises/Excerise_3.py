8a1f9909e41828949b2ebec957a86d2d7f3c3a22
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2023-11-05 13:12:35 +0400

    Update Excerise_3.py

--- Chapter 3- Data Structures/Exercises/Excerise_3.py
+++ Chapter 3- Data Structures/Exercises/Excerise_3.py
@@ -1,9 +1,9 @@
 # List of favorite cars
-favorite_cars = ['BMW M2', 'Tesla Model S', 'Porsche 911', 'Audi R8']
+favorite_cars = ['BMW M5 f10', 'BMW M5 CS', 'Porsche 911', 'Audi R8']
 # Print a statement about the first car in the list
 print(f"I would like to own a {favorite_cars[0]} series.")
 # Print a statement about the second car in the list
-print(f"I've always dreamed of driving a {favorite_cars[1]}.")
+print(f"I have always dreamed of driving a {favorite_cars[1]}.")
 # Print a statement about the third car in the list
 print(f"The {favorite_cars[2]} is a classic sports car that I admire.")
 # Print a statement about the fourth car in the list
